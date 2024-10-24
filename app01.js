a6f263ac5a58fb450b6ccd2555701bda27fd4ec6
AinhoaFalconiJo <a20230127@juan23.edu.pe>
2024-10-23 21:03:39 -0500

    Ainahoa

---
+++ app01.js
@@ -0,0 +1,4 @@
+document.write("<h1>Bienvenidos a la pagina de java script");
+var nom = prompt("Ingresa su nombre");
+document,
+  write(`Hola  ${nom}es tu primer contacto con la progamacion en Java script`);
